41a39743264c0afdc0c0b41c82e756358dcbbb56
Gabrielle <gabrielle97oliveira@gmail.com>
2024-05-16 16:17:14 +0100

    Initial Commit

---
+++ Tech News/Tech_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tech_NewsApp.swift
+//  Tech News
+//
+//  Created by Gabrielle Oliveira on 16/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Tech_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
